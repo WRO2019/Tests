eb5fc907e4f82e51180334bb85036c247e37d228
Luk3tHD <38992604+Luk3tHD@users.noreply.github.com>
2018-05-05 00:22:02 +0200

    Test
    Test

---
+++ Sensors/Main.py
@@ -0,0 +1,12 @@
+import Sensors
+import logging
+
+############################################################
+# Printing Sensors-Values
+
+while True:
+    logging.warning("IR-Angel: " + str(Sensors.get_ir_angel()))
+    logging.warning("IR-Direction: " + str(Sensors.get_ir_direction()))
+    logging.warning("IR-Distance: " + str(Sensors.get_ir_distance()))
+    logging.warning("Ultrasonis-Distance: " + str(Sensors.get_ultrasonic_distance()))
+    logging.warning("Gyron-Angel: " + str(Sensors.get_gyro_angel()))
