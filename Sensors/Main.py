08a1c7878149137ab61167910aff6ba3971d680a
Luk3tHD <38992604+Luk3tHD@users.noreply.github.com>
2018-05-05 23:43:13 +0200

    Gitgnore
    Ok

--- Sensors/Main.py
+++ Sensors/Main.py
@@ -10,4 +10,4 @@ while True:
     logging.warning("IR-Distance: " + str(Sensors.get_ir_distance()))
     logging.warning("Ultrasonis-Distance: " + str(Sensors.get_ultrasonic_distance()))
     logging.warning("Gyron-Angel: " + str(Sensors.get_gyro_angel()))
-
+    logging.warning("---------------------------------------------")
