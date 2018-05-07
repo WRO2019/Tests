0d8420672a730136d00f2cd9451fcbd78832a0f8
Kevin Koch <kevin.arthur.koch@gmail.com>
2018-05-07 17:10:38 +0200

    f bf

--- Sensors/Main.py
+++ Sensors/Main.py
@@ -11,3 +11,6 @@ while True:
     logging.warning("Ultrasonis-Distance: " + str(Sensors.get_ultrasonic_distance()))
     logging.warning("Gyron-Angel: " + str(Sensors.get_gyro_angel()))
     logging.warning("---------------------------------------------")
+
+#hallo
+#hallo
